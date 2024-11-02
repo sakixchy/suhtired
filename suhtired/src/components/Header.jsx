e03a36ad0e1537a932568e44bd829ef2bb8db3e2
sakixchy <sakibchowdhury79@gmail.com>
2024-11-02 18:01:14 +0100

    syle header, install fontawesome

--- suhtired/src/components/Header.jsx
+++ suhtired/src/components/Header.jsx
@@ -1,18 +1,25 @@
 import React from 'react'
 import logo from '../assets/images/logo/logo.jpeg'
 import styles from '../styles/Header.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faClock, faFolder, faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
 function Header() {
   return (
     <div>
         <header>
+        <div className={styles.logoContainer}>
             <img src={logo} alt="Suhtired" className={styles.logo}/>
-            <h1>Suhtired</h1>
+            <h1 className={styles.siteName}>Suhtired</h1>
+        </div>
             <nav>
                 <ul>
-                    <li><a href="#schedule">Schedule</a></li>
-                    <li><a href="#content">Content</a></li>
-                    <li><a href="#contact">Contact</a></li>
+                    <li><a href="#schedule"><FontAwesomeIcon icon={faClock} className={styles.icon} />
+                    Schedule</a></li>
+                    <li><a href="#content"><FontAwesomeIcon icon={faFolder} className={styles.icon} />
+                    Content</a></li>
+                    <li><a href="#contact"><FontAwesomeIcon icon={faEnvelope} className={styles.icon} />
+                    Contact</a></li>
                 </ul>
             </nav>
         </header>
