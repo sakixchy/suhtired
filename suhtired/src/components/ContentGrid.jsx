1f06357e7fdf86658864de7caf8337e804c5890c
sakixchy <sakibchowdhury79@gmail.com>
2024-11-03 19:53:36 +0100

    style Content gric section with headings

--- suhtired/src/components/ContentGrid.jsx
+++ suhtired/src/components/ContentGrid.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import styles from '../styles/ContentGrid.module.css'
 
 function ContentGrid() {
   return (
     <div>
-        <p>This is for content.</p>
+        <div className={styles.contentHeading}>
+            <h3 className={styles.about}>About Me</h3>
+            <h2>Meet Suhtired</h2>
+            <h3 className={styles.gamingJourney}>Gaming Journey</h3>
+        </div>
+        <div className={styles.contentText}>
+            <p>About Section</p>
+            <p>Image Slider Goes Here</p>
+            <p>Gaming Journey</p>
+        </div>
     </div>
   )
 }
